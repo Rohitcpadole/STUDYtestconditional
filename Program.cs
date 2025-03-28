8da75269fbb6f9e5049f775b474be1652e1ed232
Rohit Padole <rohitccpadole@outlook.com>
2025-03-28 08:58:31 +0530

    write hello world string

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace testconditional
 {
@@ -10,6 +7,7 @@ namespace testconditional
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
