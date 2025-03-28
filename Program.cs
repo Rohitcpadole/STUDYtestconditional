e307b79dbc585ed03f662996f59605492019c470
Rohit Padole <rohitccpadole@outlook.com>
2025-03-28 09:35:26 +0530

    update all programs

--- Program.cs
+++ Program.cs
@@ -114,6 +114,224 @@ namespace testconditional
                         Console.WriteLine("The character is not an alphabet");
                     }
                     break;
+                case 7:
+                    Console.WriteLine("Enter a number");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int sum = 0;
+                    for (int i = 1; i <= n; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine("The sum of natural numbers is " + sum);
+                    break;
+                case 8:
+                    Console.WriteLine("Enter a number");
+                    int n1 = Convert.ToInt32(Console.ReadLine());
+                    int fact = 1;
+                    for (int i = 1; i <= n1; i++)
+                    {
+                        fact *= i;
+                    }
+                    Console.WriteLine("The factorial of the number is " + fact);
+                    break;
+                case 9:
+                    Console.WriteLine("Enter a number");
+                    int n2 = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Console.WriteLine(n2 + " * " + i + " = " + n2 * i);
+                    }
+                    break;
+                case 10:
+                    Console.WriteLine("Enter the number of terms");
+                    int n3 = Convert.ToInt32(Console.ReadLine());
+                    int a = 0, b = 1, c;
+                    Console.WriteLine("Fibonacci series:");
+                    for (int i = 0; i < n3; i++)
+                    {
+                        Console.WriteLine(a);
+                        c = a + b;
+                        a = b;
+                        b = c;
+                    }
+                    break;
+                case 11:
+                    Console.WriteLine("Enter two numbers");
+                    int num4 = Convert.ToInt32(Console.ReadLine());
+                    int num5 = Convert.ToInt32(Console.ReadLine());
+                    while (num4 != num5)
+                    {
+                        if (num4 > num5)
+                        {
+                            num4 -= num5;
+                        }
+                        else
+                        {
+                            num5 -= num4;
+                        }
+                    }
+                    Console.WriteLine("The GCD of the numbers is " + num4);
+                    break;
+                case 12:
+                    Console.WriteLine("Enter two numbers");
+                    int num6 = Convert.ToInt32(Console.ReadLine());
+                    int num7 = Convert.ToInt32(Console.ReadLine());
+                    int max = Math.Max(num6, num7);
+                    int lcm = max;
+                    while (true)
+                    {
+                        if (lcm % num6 == 0 && lcm % num7 == 0)
+                        {
+                            Console.WriteLine("The LCM of the numbers is " + lcm);
+                            break;
+                        }
+                        lcm += max;
+                    }
+                    break;
+                case 13:
+                    Console.WriteLine("Enter a number");
+                    int num8 = Convert.ToInt32(Console.ReadLine());
+                    int count = 0;
+                    for (int i = 1; i <= num8; i++)
+                    {
+                        if (num8 % i == 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine("The number is a prime number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not a prime number");
+                    }
+                    break;
+                case 14:
+                    Console.WriteLine("Enter a number");
+                    int num9 = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 2; i <= num9; i++)
+                    {
+                        int count1 = 0;
+                        for (int j = 1; j <= i; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                count1++;
+                            }
+                        }
+                        if (count1 == 2)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    break;
+                case 15:
+                    Console.WriteLine("Enter a number");
+                    int num10 = Convert.ToInt32(Console.ReadLine());
+                    int sum1 = 0;
+                    int temp = num10;
+                    while (num10 > 0)
+                    {
+                        int rem = num10 % 10;
+                        sum1 += rem * rem * rem;
+                        num10 /= 10;
+                    }
+                    if (sum1 == temp)
+                    {
+                        Console.WriteLine("The number is an Armstrong number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not an Armstrong number");
+                    }
+                    break;
+                case 16:
+                    Console.WriteLine("Enter a number");
+                    int num11 = Convert.ToInt32(Console.ReadLine());
+                    int sum2 = 0;
+                    int temp1 = num11;
+                    while (num11 > 0)
+                    {
+                        int rem = num11 % 10;
+                        sum2 = sum2 * 10 + rem;
+                        num11 /= 10;
+                    }
+                    if (sum2 == temp1)
+                    {
+                        Console.WriteLine("The number is a palindrome number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not a palindrome number");
+                    }
+                    break;
+                case 17:
+                    Console.WriteLine("Enter a number");
+                    int num12 = Convert.ToInt32(Console.ReadLine());
+                    int sum3 = 0;
+                    for (int i = 1; i < num12; i++)
+                    {
+                        if (num12 % i == 0)
+                        {
+                            sum3 += i;
+                        }
+                    }
+                    if (sum3 == num12)
+                    {
+                        Console.WriteLine("The number is a perfect number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not a perfect number");
+                    }
+                    break;
+                case 18:
+                    Console.WriteLine("Enter a number");
+                    int num13 = Convert.ToInt32(Console.ReadLine());
+                    int sum4 = 0;
+                    int temp2 = num13;
+                    while (num13 > 0)
+                    {
+                        int rem = num13 % 10;
+                        int fact1 = 1;
+                        for (int i = 1; i <= rem; i++)
+                        {
+                            fact1 *= i;
+                        }
+                        sum4 += fact1;
+                        num13 /= 10;
+                    }
+                    if (sum4 == temp2)
+                    {
+                        Console.WriteLine("The number is a strong number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not a strong number");
+                    }
+                    break;
+                case 19:
+                    Console.WriteLine("Enter a number");
+                    int num14 = Convert.ToInt32(Console.ReadLine());
+                    int sum5 = 0;
+                    int temp3 = num14;
+                    while (num14 > 0)
+                    {
+                        int rem = num14 % 10;
+                        sum5 += rem * rem;
+                        num14 /= 10;
+                    }
+                    if (sum5 == 1)
+                    {
+                        Console.WriteLine("The number is a happy number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is not a happy number");
+                    }
+                    break;
                 default:
                     Console.WriteLine("Invalid choice");
                     break;
